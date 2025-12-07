72894ccae8b30f0c39de22b7d7efebcce27536dc
jyotsna1614 <jyotsnabhartiya27@gmail.com>
2025-12-07 18:21:39 +0530

    newww branch

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/share/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/abhayindwar/Desktop/SIH25082.nosync"
+export "FLUTTER_ROOT=C:\flutter_windows_3.35.3-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\jyots\trackmatee"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/opt/homebrew/share/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/abhayindwar/Desktop/SIH25082.nosync"
+export "FLUTTER_ROOT=C:\flutter_windows_3.35.3-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\jyots\trackmatee"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
